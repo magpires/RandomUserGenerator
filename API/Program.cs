6ec7569398eeb99bc3f13d1453e51d30758f2f08
Weverton Pires <30535993+magpires@users.noreply.github.com>
2024-06-20 15:02:00 -0300

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Program
@@ -5,6 +8,9 @@ namespace API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<RandomUserGeneratorContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 
             // Add services to the container.
 
